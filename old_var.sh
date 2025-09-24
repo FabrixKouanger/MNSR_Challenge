39d97bcdc42082f17b8bfc6c272b3481d9f9a055
Fabrice Kouanga <66472802+FabrixKouanger@users.noreply.github.com>
2025-09-24 11:46:05 +0200

    Delete old_var.sh

--- old_var.sh
+++
@@ -1,6 +0,0 @@
-# Variables cryptées laissées par l'ancien admin
-export USER1_CRYPT="cm9vdG1lX3VzZXIx"
-export USER2_CRYPT="cm9vdG1lX3VzZXIy"
-export GROUP_CRYPT="c2VjdXJpdHlfZ3Jw"
-export ADMIN_CRYPT="YWRtaW5fdXNlcg=="
-export SECRET_FILE_CRYPT="c2VjcmV0X2ZpbGUudHh0"
